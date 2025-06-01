81e355b2e40934d88fb427e468b295de29dc7556
NotAKrayer <157698427+NotAKrayer@users.noreply.github.com>
2025-06-01 20:24:26 +0500

    0.4.1

--- scripts/number.js
+++ scripts/number.js
@@ -11,27 +11,27 @@ let player = {
 
 let tierUpData = [ //cost - bro. need  - needed tier level for stuff. sign - bro...
     {
-        cost: 0,
+        cost: new Decimal(0),
         need: 0,
         sign: "ℕ"
     },
     {
-        cost: 15,
+        cost: new Decimal(15),
         need: 1,
         sign: "ℤ"
     },
     {
-        cost: 50,
+        cost: new Decimal(50),
         need: 2,
         sign: "ℚ"
     },
     {
-        cost: 150,
+        cost: new Decimal(150),
         need: 3,
         sign: "ℝ"
     },
     {
-        cost: 300,
+        cost: new Decimal(300),
         need: 4,
         sign: "ℂ"
     }
@@ -41,17 +41,25 @@ let tierUpData = [ //cost - bro. need  - needed tier level for stuff. sign - bro
 createObject(900, 500,
     `
     <div style="width: 550px; height: 500px; background-color: #000000; border: 1px solid white";>
-    <p style="font-size: 25px; text-align: center; margin: auto;" class="purplegrad">∞ Number ∞<br> Current Tier: <span id="tier"">None</span> <br> <br>  <span id="number">0</span> </p>
+    <p style="font-size: 24px; text-align: center; margin: auto; word-break: break-word;" class="purplegrad">∞ Number ∞<br> Current Tier: <span id="tier"">None</span> </p>
     <button class="info1" onclick="openInfo()">i</button>
     <button id="tierUp" onclick="tierUp()">Tier Up <br> Reset your <span class="purplegrad">Number</span>, but give boost to your <span class="purplegrad">Number</span> gain <br> Cost: <span id="costTier">0</span></button>
     </div>
     `
 );
-createObject(355, 350,
+createObject(350, 350,
     `
     <div style="width: 500px; height: 200px; background-color: #000000; border: 1px solid white"; id="info1">
     <p style="font-size: 25px; text-align: center; margin: auto; color: wheat;">NUMBER</p>
     <p style="font-size: 18px; text-align: left; margin-left: 10px; color: white;">Current Boost by tier: <span id="tierBoost">0</span>x</p>
+    </div>
+    `
+);
+
+createObject(900, 210,
+    `
+    <div style="width: 550px; height: 50px; background-color: #000000; border: 1px solid white";>
+    <p style="font-size: 25px; text-align: center; margin-top: 10px;" class="purplegrad";"><span id="number">0</span></p>
     </div>
     `
 ); //PLEASE ADD R^3 STUFF
@@ -84,23 +92,27 @@ function openInfo() {
 }
 
 function tierUp() {
-    let currentTier = player.number.tier.toNumber();
-    let currentTierNext = currentTier + 1;
+    let currentTier = player.number.tier;
+    let currentTierNext = (currentTier).add("1e0");
     if (new Decimal(player.number.tier).gte("5")) {
         if (new Decimal(player.number.tier).gte(new Decimal("5e0"))) {
             if (new Decimal(player.number.value).gte(player.number.numSys)) {
                 player.number.numSys = new Decimal(player.number.numSys).mul(1.5);
-                el("costTier").innerHTML = (player.number.numSys).toPrecision(3)
+                formatNumber("costTier", player.number.numSys)
                 player.number.tier = new Decimal(player.number.tier).add("1e0") //add tier
-                el("tier").innerHTML = capitalizeFirst(cleanOutput(
+                //
+                 let tierName = capitalizeFirst(cleanOutput(
                     numberToLatin(player.number.tier.toString(), { la_macra: false, la_useard: false, ___showsep: false }, latinTables.latinmacron)
                     .replaceAll("<wbr/>", "")
-                    )
-                ) + "nions"  + `(ℝ^${(new Decimal(currentTier).add("1e0")).toPrecision(3)})`
+                ))
+                const maxLength = 45;
+                let displayedTierName = tierName.length > maxLength ? tierName.slice(0, maxLength - 3) + "..." : tierName;
+                el("tier").innerHTML = displayedTierName + "nions" + `(ℝ^${(player.number.tier).toPrecision(3)})`;;
+                ///
                 player.number.tierBoost = new Decimal(player.number.tierBoost).add("1e0") //adding boost
-                el("tierBoost").innerHTML = player.number.tierBoost.toPrecision(3) //visual
+                formatNumber("tierBoost", player.number.tierBoost)
                 player.number.value = new Decimal("0e0")
-                el("number").innerHTML = player.number.value //visual
+                formatNumber("number", player.number.value)
             }
         }
     } else {
@@ -109,14 +121,14 @@ function tierUp() {
                 if (currentTierNext != 5) {
                     el("costTier").innerHTML = tierUpData[currentTierNext].cost.toPrecision(3)
                 } else {
-                    el("costTier").innerHTML = (player.number.numSys).toPrecision(3)
+                    formatNumber("costTier", player.number.numSys)
                 }
                 player.number.tier = new Decimal(player.number.tier).add("1e0") //add tier
                 el("tier").innerHTML = tierUpData[currentTier].sign //visual
                 player.number.tierBoost = new Decimal(player.number.tierBoost).add("1e0") //adding boost
-                el("tierBoost").innerHTML = player.number.tierBoost.toPrecision(3) //visual
+                formatNumber("tierBoost", player.number.tierBoost) //visual
                 player.number.value = new Decimal("0e0")
-                el("number").innerHTML = player.number.value //visual
+                formatNumber("number", player.number.value) //visual
             }
         }
     }
@@ -124,10 +136,10 @@ function tierUp() {
 
 function gainNumber(delta_time, total_time) {
     const e = new Decimal(1)
-        .mul(player.number.tierBoost).mul(1e34)
+        .mul(player.number.tierBoost).mul(43)
         .mul(delta_time / 1000)
     player.number.value = new Decimal(player.number.value).plus(e)
-    el("number").innerHTML = player.number.value.toPrecision(3);
+    formatNumber("number", player.number.value)
 }
 
 let last_time = null;
